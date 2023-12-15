202b90a573fe3d1b8e3d094931cde9e89e67de46
cybiz <49467492+cybiz-tva@users.noreply.github.com>
2023-12-15 15:07:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
-import asyncio
 import logging
 import os
-from datetime import datetime, timedelta
-
-import uvloop
+import asyncio
 from pyrogram import Client, filters
-from pyrogram.raw import functions
-from pyrogram.raw.types import UpdateNewMessage, UpdateMessageID, UpdateMessageEdited
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 logging.basicConfig(level=logging.WARNING, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
@@ -15,55 +10,65 @@ logging.getLogger("pyrogram").setLevel(logging.WARNING)
 API_ID = int(os.getenv("API_ID"))
 API_HASH = os.getenv("API_HASH")
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-TARGET_EMOJI = "👍"  # Replace with the emoji you want to track
-
-uvloop.install()
+TARGET_EMOJI = "👍"
 
 bot = Client(name="reactiontrackerbot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 
 logging.warning("⚡️ Bot Started!")
 
+async def mark_user_engaged(chat_id, user_id):
+    # Implement your logic for marking a user as engaged
+    pass
 
-@bot.on_message(filters.command("start") & filters.private)
-async def start_bot(cl, m):
-    keyboard = InlineKeyboardMarkup([
-        [InlineKeyboardButton(text="React to this message", callback_data="react")]
-    ])
-    await m.reply("Hello! React to the message with the specified emoji to be tracked.", reply_markup=keyboard)
+async def check_inactive_user(chat_id, user_id):
+    # Implement your logic for checking inactive users
+    pass
 
-
-@bot.on_callback_query(filters.regex("react"))
-async def react_callback(_, cq):
-    message = cq.message
-    chat_id = message.chat.id
-    message_id = message.message_id
-
-    # Send the message to be tracked
-    tracked_message = await bot.send_message(chat_id, "This message is being tracked. React to it with the specified emoji.")
-
-    # Store the tracked message ID for future reference
+@bot.on_message(filters.command("track_message"))
+async def track_message_command(_, m):
+    chat_id = m.chat.id
+    tracked_message = await bot.send_message(
+        chat_id,
+        "React to this message to be considered engaged:",
+        reply_markup=InlineKeyboardMarkup([[
+            InlineKeyboardButton(text=" Engage", callback_data="engaged"),
+            InlineKeyboardButton(text="➖ Not interested", callback_data="not_interested"),
+        ]]),
+    )
     await bot.set_database(chat_id, {"tracked_message_id": tracked_message.message_id})
 
+@bot.on_callback_query(filters.regex("^(engaged|not_interested)$"))
+async def track_button_callback(_, cq):
+    chat_id = cq.message.chat.id
+    user_id = cq.from_user.id
+    action = cq.data
+    await cq.answer()
+    if action == "engaged":
+        await mark_user_engaged(chat_id, user_id)
+    elif action == "not_interested":
+        await cq.edit_message_text("Okay, you won't receive further engagement prompts.")
+        # Consider adding confirmation or opting-out mechanism
 
-@bot.on_raw_update()
-async def on_raw_update(_, update, users, chat_id):
-    if isinstance(update, UpdateMessageID) or isinstance(update, UpdateNewMessage) or isinstance(update, UpdateMessageEdited):
-        if hasattr(update, 'message'):
-            message = update.message
-            if hasattr(message, 'reactions') and len(message.reactions) > 0:
-                for reaction in message.reactions:
-                    if reaction.reaction == TARGET_EMOJI:
-                        user_id = reaction.user_id
-                        # Remove the user or take any other action
-                        await remove_user(chat_id, user_id)
+async def main():
+    while True:
+        chat_id = bot.db.get("chat_id")  # Replace with your actual chat_id
+        tracked_message_id = bot.db.get("tracked_message_id")
+        
+        if chat_id and tracked_message_id:
+            try:
+                message = await bot.get_messages(chat_id, message_ids=[tracked_message_id])
+                for user_id, reaction in message.reactions.items():
+                    if reaction == TARGET_EMOJI:
+                        await mark_user_engaged(chat_id, user_id)
+                    elif reaction in (None, "", TARGET_EMOJI + "-"):
+                        await check_inactive_user(chat_id, user_id)
+            except Exception as e:
+                logging.error(f"Error processing reactions: {e}")
 
+        await asyncio.sleep(60)  # Adjust polling interval as needed
 
-async def remove_user(chat_id, user_id):
-    try:
-        # Your code to remove or take action on the user
-        await bot.kick_chat_member(chat_id, user_id)
-    except Exception as e:
-        logging.error(f"Error removing user: {e}")
-
-
-bot.run()
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    loop.create_task(bot.start())
+    loop.create_task(main())
+    loop.run_forever()
