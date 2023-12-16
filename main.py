e21f850f079bc9a27349031d993c3ebbc6cdc7ae
cybiz <49467492+cybiz-tva@users.noreply.github.com>
2023-12-16 16:40:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
-import asyncio
 import logging
 import os
-from datetime import datetime, timedelta
-
-import uvloop
 from pyrogram import Client, filters
-from pyrogram.enums import ChatType, ChatMemberStatus
-from pyrogram.errors import FloodWait
-from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+from pyrogram.types import Message
+from datetime import datetime, timedelta
+import asyncio
 
 logging.basicConfig(level=logging.WARNING, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
@@ -16,114 +12,48 @@ API_ID = int(os.getenv("API_ID"))
 API_HASH = os.getenv("API_HASH")
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
-uvloop.install()
-
 bot = Client(name="kickmemberbot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 
 logging.warning("⚡️ Bot Started!")
-
-
-@bot.on_message(filters.command("start") & filters.private)
-async def start_bot(cl: Client, m: Message):
-    keyboard = InlineKeyboardMarkup([
-        [InlineKeyboardButton(text="➕ Add me to a group",
-                              url=f"tg://resolve?domain={cl.me.username}&startgroup=&admin=manage_chat+restrict_members")],
-        [InlineKeyboardButton(text="➕ Add me to a channel",
-                              url=f"tg://resolve?domain={cl.me.username}&startchannel&admin=change_info+restrict_members+post_messages")],
-        [InlineKeyboardButton(text="📦 Public Repository", url="https://github.com/samuelmarc/kickallmembersbot")]
-    ])
-    await m.reply(
-        f"Hello {m.from_user.mention} I am a bot to remove (not ban) all users from your group or channel created by @samuel_ks, below you can add the bot to your group or channel or access the bot's public repository.",
-        reply_markup=keyboard)
-
-
-@bot.on_message(filters.command("help"))
-async def help_bot(_, m: Message):
-    await m.reply(
-        "Need help? To use the bot it's very simple, just add me to your group or channel as an admin and use the /kick_all command and all users will be removed (not banned).")
-
-
-@bot.on_message(filters.command("kick_all") & (filters.channel | filters.group))
-async def kick_all_members(cl: Client, m: Message):
-    chat = await cl.get_chat(chat_id=m.chat.id)
-    my = await chat.get_member(cl.me.id)
-    if my.privileges:
-        if my.privileges.can_manage_chat and my.privileges.can_restrict_members:
-            is_channel = True if m.chat.type == ChatType.CHANNEL else False
-            if not is_channel:
-                req_user_member = await chat.get_member(m.from_user.id)
-                if req_user_member.privileges is None:
-                    await m.reply("❌ You are not an admin and cannot execute this command!")
-                    return
-            kick_count = 0
-            members_count = chat.members_count
-            if members_count <= 200:
-                async for member in chat.get_members():
-                    if member.user.id == cl.me.id:
-                        continue
-                    elif member.status == ChatMemberStatus.ADMINISTRATOR or member.status == ChatMemberStatus.OWNER:
-                        continue
-                    try:
-                        await chat.ban_member(member.user.id, datetime.now() + timedelta(seconds=30))
-                        kick_count += 1
-                    except FloodWait as e:
-                        await asyncio.sleep(e.value)
-                await m.reply(f"✅ Total Users Removed: {kick_count}")
-            else:
-                loops_count = members_count / 200
-                loops_count = round(loops_count)
-                for loop_num in range(loops_count):
-                    async for member in chat.get_members():
-                        if member.user.id == cl.me.id:
-                            continue
-                        elif member.status == ChatMemberStatus.ADMINISTRATOR or member.status == ChatMemberStatus.OWNER:
-                            continue
-                        try:
-                            await chat.ban_member(member.user.id, datetime.now() + timedelta(seconds=30))
-                            kick_count += 1
-                        except FloodWait as e:
-                            await asyncio.sleep(e.value)
-                    await asyncio.sleep(15)
-                await m.reply(f"✅ Total Users Removed: {kick_count}")
-        else:
-            await m.reply("❌ The bot is an admin but does not have the necessary permissions!")
-    else:
-        await m.reply("❌ The bot must have admin!")
-
 
 @bot.on_message(filters.command("remove") & (filters.channel | filters.group))
 async def remove_members(cl: Client, m: Message):
     chat = await cl.get_chat(chat_id=m.chat.id)
     my = await chat.get_member(cl.me.id)
-    if my.privileges:
-        if my.privileges.can_manage_chat and my.privileges.can_restrict_members:
-            is_channel = True if m.chat.type == ChatType.CHANNEL else False
-            if not is_channel:
-                req_user_member = await chat.get_member(m.from_user.id)
-                if req_user_member.privileges is None:
-                    await m.reply("❌ You are not an admin and cannot execute this command!")
-                    return
-
-            user_ids = m.text.split()[1:]
-            if not user_ids:
-                await m.reply("❌ Please provide user IDs to remove.")
+    if my.status in ["administrator", "creator"]:
+        is_channel = m.chat.type == "channel"
+        if not is_channel:
+            req_user_member = await chat.get_member(m.from_user.id)
+            if req_user_member.status not in ["administrator", "creator"]:
+                await m.reply("❌ You are not an admin and cannot execute this command!")
                 return
 
-            kick_count = 0
-            for user_id in user_ids:
-                try:
-                    await chat.ban_member(int(user_id), datetime.now() + timedelta(seconds=30))
+        user_ids = m.text.split()[1:]
+        if not user_ids:
+            await m.reply("❌ Please provide user IDs to remove.")
+            return
+
+        kick_count = 0
+        for user_id in user_ids:
+            try:
+                resp = await kick_chat_member(chat.id, int(user_id))
+                if resp and resp.ok:
                     kick_count += 1
-                except FloodWait as e:
-                    await asyncio.sleep(e.value)
-                except Exception as e:
-                    await m.reply(f"❌ Failed to remove user with ID {user_id}: {str(e)}")
+                else:
+                    await m.reply(f"❌ Failed to remove user with ID {user_id}.")
+            except Exception as e:
+                await m.reply(f"❌ Failed to remove user with ID {user_id}: {str(e)}")
 
-            await m.reply(f"✅ Total Users Removed: {kick_count}")
-        else:
-            await m.reply("❌ The bot is an admin but does not have the necessary permissions!")
+        await m.reply(f"✅ Total Users Removed: {kick_count}")
     else:
-        await m.reply("❌ The bot must have admin!")
+        await m.reply("❌ The bot is not an admin or does not have the necessary permissions!")
 
+async def kick_chat_member(chat_id, user_id):
+    # PipedreamHQ library method implementation
+    try:
+        resp = await bot.telegram_bot_api.kick_chat_member(chat_id, user_id)
+        return resp
+    except Exception as e:
+        raise e
 
 bot.run()
