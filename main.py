b8a54c75a8664f8dc24346f69b992846c762bb35
cybiz <49467492+cybiz-tva@users.noreply.github.com>
2023-12-19 18:51:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,18 @@
+import os
 import telebot
 import re
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 from datetime import datetime
 
-# Replace with your Telegram bot token
-TOKEN = '5999713069:AAFYhnSI-79OCEEsnQL98K581QkUePq6T-8'
+# Get sensitive information from environment variables
+TOKEN = os.environ.get('BOT_TOKEN')
+CREDENTIALS_JSON_PATH = os.environ.get('GOOGLE_SHEETS_CREDENTIALS_PATH')
 
 bot = telebot.TeleBot(TOKEN)
 
-# Create an empty dictionary to store user responses
 user_data = {}
 
-# Replace with the path to your Google Sheets credentials JSON file
-CREDENTIALS_JSON_PATH = 'D:/Downloads/mikfxdatabase-965088f7d3f9.json'
-
-# URLs for images
 WELCOME_IMAGE_URL = 'https://ibb.co/zXdD7M0'
 SUCCESS_IMAGE_URL = 'https://ibb.co/m9C7h29'
 INVALID_INPUT_IMAGE_URL = 'https://ibb.co/VHrqD1Y'
@@ -25,16 +22,14 @@ SUBMIT_MOBILE_NUMBER_IMAGE_URL = 'https://ibb.co/rvZYJ76'
 @bot.message_handler(commands=['start'])
 def handle_start(message):
     user_id = message.from_user.id
-    user_data[user_id] = {}  # Initialize user data dictionary for the user
+    user_data[user_id] = {}
 
-    # Get user details (name, username, user ID)
     name = message.from_user.first_name
     username = message.from_user.username
     user_data[user_id]['name'] = name
     user_data[user_id]['username'] = username
     user_data[user_id]['user_id'] = user_id
 
-    # Send welcome message with image
     bot.send_photo(user_id, WELCOME_IMAGE_URL, caption=f"Welcome, {name}! Click on the button to submit your details:",
                    reply_markup=create_submit_button())
 
@@ -62,7 +57,7 @@ def get_mobile_number(message):
     chat_id = message.chat.id
 
     if 'mobile_number' not in user_data[user_id]:
-        if re.match(r"^\d+$", message.text):  # Check if the input is digits only
+        if re.match(r"^\d+$", message.text):
             user_data[user_id]['mobile_number'] = message.text
             bot.send_photo(chat_id, SUBMIT_EXNESS_ID_IMAGE_URL, caption="Submit your Exness ID (digits only):",
                            reply_markup=create_submit_exness_id_button())
@@ -82,14 +77,11 @@ def get_exness_id(message):
     chat_id = message.chat.id
 
     if 'exness_id' not in user_data[user_id]:
-        if re.match(r"^\d+$", message.text):  # Check if the input is digits only
+        if re.match(r"^\d+$", message.text):
             user_data[user_id]['exness_id'] = message.text
-
-            # Save user data to Google Sheets
             save_user_data_to_google_sheets(user_data[user_id])
-
             bot.send_photo(chat_id, SUCCESS_IMAGE_URL, caption="Thank you! Your details have been submitted and saved.")
-            user_data.pop(user_id)  # Remove user data from dictionary after submission
+            user_data.pop(user_id)
         else:
             send_invalid_input_message(chat_id)
             bot.register_next_step_handler(message, get_exness_id)
@@ -100,30 +92,24 @@ def send_invalid_input_message(chat_id):
 
 def save_user_data_to_google_sheets(user_data):
     try:
-        # Load credentials from the Google Sheets credentials JSON file
         scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
         credentials = ServiceAccountCredentials.from_json_keyfile_name(CREDENTIALS_JSON_PATH, scope)
         gc = gspread.authorize(credentials)
 
-        # Open the Google Sheets document by title
         spreadsheet = gc.open("Mikfx Database")
-
-        # Select the first sheet
         sheet = spreadsheet.get_worksheet(0)
 
-        # Get the next available row
         next_row = len(sheet.col_values(1)) + 1
 
-        # Write data to Google Sheets
-        sheet.update_cell(next_row, 1, str(datetime.now()))  # Date & time
-        sheet.update_cell(next_row, 2, user_data['name'])  # Name
-        sheet.update_cell(next_row, 3, user_data['username'])  # Username
-        sheet.update_cell(next_row, 4, user_data['user_id'])  # User ID
-        sheet.update_cell(next_row, 5, user_data['mobile_number'])  # Mobile number
-        sheet.update_cell(next_row, 6, user_data['exness_id'])  # Exness ID
+        sheet.update_cell(next_row, 1, str(datetime.now()))
+        sheet.update_cell(next_row, 2, user_data['name'])
+        sheet.update_cell(next_row, 3, user_data['username'])
+        sheet.update_cell(next_row, 4, user_data['user_id'])
+        sheet.update_cell(next_row, 5, user_data['mobile_number'])
+        sheet.update_cell(next_row, 6, user_data['exness_id'])
 
     except Exception as e:
         print(f"Error saving user data to Google Sheets: {e}")
 
-if _name_ == "_main_":
+if __name__ == "__main__":
     bot.polling(none_stop=True)
